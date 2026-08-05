eccd33cab6b771ee74812f7996b2c65590fcce76
Kenneth Hoste <kenneth.hoste@ugent.be>
2026-08-05 13:50:50 +0200

    fix test_toy_build_hooks

--- test/framework/toy_build.py
+++ test/framework/toy_build.py
@@ -3679,9 +3679,12 @@ class ToyBuildTest(EnhancedTestCase):
             ['%(name)s-%(version)s.tar.gz']
             echo toy
             in module-write hook hook for {mod_name}
+            in module-write hook hook for {mod_name}
+            in module-write hook hook for {mod_name}
             installing of extension bar is done!
             pre_run_shell_cmd_hook triggered for ' gcc toy.c -o toy '
             ' gcc toy.c -o toy  && copy_toy_file toy copy_of_toy' command failed (exit code 127), but I fixed it!
+            in module-write hook hook for {mod_name}
             installing of extension toy is done!
             pre_sanity_check_hook
             in module-write hook hook for {mod_name}
