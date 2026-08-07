2cda5bcaa40e4be13b33c94a4328536c22f3359c
Kenneth Hoste <kenneth.hoste@ugent.be>
2026-08-07 16:31:48 +0200

    add test_toy_exts_sequential

--- test/framework/toy_build.py
+++ test/framework/toy_build.py
@@ -1966,9 +1966,9 @@ class ToyBuildTest(EnhancedTestCase):
             self.eb_main([test_ec, '--module-only', '--force'], do_build=True, raise_error=True)
         self.assertExists(toy_mod)
 
-    def test_toy_exts_parallel(self):
+    def _test_toy_exts_common(self, args=None):
         """
-        Test parallel installation of extensions (--parallel-extensions-install)
+        Common code for test_toy_exts_sequential and test_toy_exts_parallel tests
         """
         toy_mod = os.path.join(self.test_installpath, 'modules', 'all', 'toy', '0.0')
         if get_module_syntax() == 'Lua':
@@ -1997,25 +1997,100 @@ class ToyBuildTest(EnhancedTestCase):
         ])
         write_file(test_ec, test_ec_txt)
 
-        args = ['--parallel-extensions-install', '--experimental', '--force', '--parallel=3']
-        stdout, stderr = self.run_test_toy_build_with_output(ec_file=test_ec, extra_args=args, raise_error=True,
-                                                             versionsuffix='-GCC-12.3.0')
+        extra_args = ['--force', '--parallel=3']
+        if args:
+            extra_args.extend(args)
+
+        write_file(self.logfile, '')
+
+        stdout, stderr = self.run_test_toy_build_with_output(ec_file=test_ec, versionsuffix='-GCC-12.3.0',
+                                                             extra_args=extra_args, raise_error=True)
         self.assertEqual(stderr, '')
+
+        logtxt = read_file(self.logfile)
+
+        return logtxt
+
+    def test_toy_exts_sequential(self):
+        """
+        Test sequential installation of extensions (--disable-parallel-extensions-install)
+        """
+        # currently --parallel-extensions-install is disabled by default,
+        # but also test with it disable explicitly
+        for args in ([], ['--disable-parallel-extensions-install']):
+
+            logtxt = self._test_toy_exts_common(args=args)
+
+            self.assertRegex(logtxt, "INFO Installing extensions sequentially")
+
+            patterns = [f"INFO installing extension {x}" for x in ('ls', 'bar', 'barbar', 'toy')]
+            self.assert_multi_regex(patterns, logtxt)
+
+            # check how many time fake module is loaded;
+            # should be 6 times:
+            # - three times in extensions step (by EasyBlock._install_extensions_det_init_build_env)
+            #   - once at start (before loop)
+            #   - twice because changes to fake module were detected (after installing of bar & barbar extensions)
+            # - twice in sanity check step:
+            #   - once for toy extension, via sanity_check_module_environment in ExtensionEasyBlock.sanity_check_step
+            #   - once via sanity_check_load_module in EasyBlock._sanity_check_step
+            # - once in module step (when creating devel module)
+            regex_load_fake_mod = re.compile("INFO Loading fake module", re.M)
+            self.assertEqual(len(regex_load_fake_mod.findall(logtxt)), 6)
+
+            # count number of 'module load' commands that were run
+            regex_module_load = re.compile("INFO Running command.*\n.* python load (.*)", re.M)
+            res = regex_module_load.findall(logtxt)
+            # there should be 5 'module load' commands in total
+            expected = [
+                # one load command for toolchain + all dependencies
+                "GCC/12.3.0 binutils/2.40-GCCcore-12.3.0 OpenMPI/4.1.5-GCC-12.3.0",
+                # three times a load command for fake module + build dependencies
+                # (via EasyBlock.install_extensions_parallel)
+                "binutils/2.40-GCCcore-12.3.0 toy/0.0-GCC-12.3.0",
+                "binutils/2.40-GCCcore-12.3.0 toy/0.0-GCC-12.3.0",
+                "binutils/2.40-GCCcore-12.3.0 toy/0.0-GCC-12.3.0",
+                # two load commands in sanity check step (once for 'toy' extension, once for top-level)
+                "toy/0.0-GCC-12.3.0",
+                "toy/0.0-GCC-12.3.0",
+                # one load command for module step (when creating devel module)
+                "toy/0.0-GCC-12.3.0",
+            ]
+            self.assertEqual(res, expected)
+
+            # also test skipping of extensions in parallel
+            args.append('--skip')
+
+            logtxt = self._test_toy_exts_common(args=args)
+
+            # order in which these patterns occur is not fixed, so check them one by one
+            patterns = [
+                r"INFO skipping installed extensions \(sequentially\)$",
+                r"INFO skipping extension ls$",
+                r"INFO skipping extension bar$",
+                r"INFO skipping extension barbar$",
+                r"INFO skipping extension toy$",
+            ]
+            self.assert_multi_regex(patterns, logtxt)
+
+    def test_toy_exts_parallel(self):
+        """
+        Test parallel installation of extensions (--parallel-extensions-install)
+        """
+        args = ['--parallel-extensions-install']
+
+        logtxt = self._test_toy_exts_common(args=args)
 
         # take into account that each of these lines may appear multiple times,
         # in case no progress was made between checks
         patterns = [
-            r"== 1 out of 4 extensions installed \(2 queued, 1 running: bar\)$",
-            r"== 2 out of 4 extensions installed \(1 queued, 1 running: barbar\)$",
-            r"== 3 out of 4 extensions installed \(0 queued, 1 running: toy\)$",
-            r"== 4 out of 4 extensions installed \(0 queued, 0 running: \)$",
+            "INFO Installing extensions in parallel",
+            r"INFO 1 out of 4 extensions installed \(2 queued, 1 running: bar\)$",
+            r"INFO 2 out of 4 extensions installed \(1 queued, 1 running: barbar\)$",
+            r"INFO 3 out of 4 extensions installed \(0 queued, 1 running: toy\)$",
+            r"INFO 4 out of 4 extensions installed \(0 queued, 0 running: \)$",
         ]
-        for pattern in patterns:
-            regex = re.compile(pattern, re.M)
-            error_msg = "Expected pattern '%s' should be found in %s'" % (regex.pattern, stdout)
-            self.assertTrue(regex.search(stdout), error_msg)
-
-        logtxt = read_file(self.logfile)
+        self.assert_multi_regex(patterns, logtxt)
 
         # check how many time fake module is loaded;
         # should be 4 times:
@@ -2046,22 +2121,18 @@ class ToyBuildTest(EnhancedTestCase):
 
         # also test skipping of extensions in parallel
         args.append('--skip')
-        stdout, stderr = self.run_test_toy_build_with_output(ec_file=test_ec, extra_args=args, raise_error=True,
-                                                             versionsuffix='-GCC-12.3.0')
-        self.assertEqual(stderr, '')
+
+        logtxt = self._test_toy_exts_common(args=args)
 
         # order in which these patterns occur is not fixed, so check them one by one
         patterns = [
-            r"^== skipping installed extensions \(in parallel\)$",
-            r"^== skipping extension ls$",
-            r"^== skipping extension bar$",
-            r"^== skipping extension barbar$",
-            r"^== skipping extension toy$",
+            r"INFO skipping installed extensions \(in parallel\)$",
+            r"INFO skipping extension ls$",
+            r"INFO skipping extension bar$",
+            r"INFO skipping extension barbar$",
+            r"INFO skipping extension toy$",
         ]
-        for pattern in patterns:
-            regex = re.compile(pattern, re.M)
-            error_msg = "Expected pattern '%s' should be found in %s'" % (regex.pattern, stdout)
-            self.assertTrue(regex.search(stdout), error_msg)
+        self.assert_multi_regex(patterns, logtxt)
 
         # check behaviour when using Toy_Extension easyblock that doesn't implement required_deps method;
         # framework should fall back to installing extensions sequentially
@@ -2073,22 +2144,19 @@ class ToyBuildTest(EnhancedTestCase):
         write_file(toy_ext_eb, toy_ext_eb_txt)
 
         args[-1] = '--include-easyblocks=%s' % toy_ext_eb
-        stdout, stderr = self.run_test_toy_build_with_output(ec_file=test_ec, extra_args=args, raise_error=True,
-                                                             versionsuffix='-GCC-12.3.0')
-        self.assertEqual(stderr, '')
+
+        logtxt = self._test_toy_exts_common(args=args)
+
         # take into account that each of these lines may appear multiple times,
         # in case no progress was made between checks
         patterns = [
-            r"^== 1 out of 4 extensions installed \(2 queued, 1 running: bar\)$",
-            r"^== 2 out of 4 extensions installed \(1 queued, 1 running: barbar\)$",
-            r"^== 3 out of 4 extensions installed \(0 queued, 1 running: toy\)$",
-            r"^== 4 out of 4 extensions installed \(0 queued, 0 running: \)$",
+            r"INFO 1 out of 4 extensions installed \(2 queued, 1 running: bar\)$",
+            r"INFO 2 out of 4 extensions installed \(1 queued, 1 running: barbar\)$",
+            r"INFO 3 out of 4 extensions installed \(0 queued, 1 running: toy\)$",
+            r"INFO 4 out of 4 extensions installed \(0 queued, 0 running: \)$",
             '',
         ]
-        for pattern in patterns:
-            regex = re.compile(pattern, re.M)
-            error_msg = "Expected pattern '%s' should be found in %s'" % (regex.pattern, stdout)
-            self.assertTrue(regex.search(stdout), error_msg)
+        self.assert_multi_regex(patterns, logtxt)
 
     def test_backup_modules(self):
         """Test use of backing up of modules with --module-only."""
